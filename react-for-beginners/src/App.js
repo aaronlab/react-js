1d75a2ffb1aa499aef78e592401cfaf667984b09
Aaron Lee <aaronlab.net@gmail.com>
2023-09-10 18:27:09 +0900

    effect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,44 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => (prev += 1));
+
+  const [keyword, setKeyword] = useState("");
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("App rendered");
+
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search FOR", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("Counter changed", counter);
+  }, [counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here"
+      />
+
       <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Button"} />
+
+      <p>{counter}</p>
+
+      <Button text={"Click Me"} onClick={onClick} />
     </div>
   );
 }
--- react-for-beginners/src/Button.js
+++ react-for-beginners/src/Button.js
@@ -1,12 +1,19 @@
 import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
+function Button({ text, onClick }) {
+  console.log("Button Rendered!");
+
+  return (
+    <button className={styles.btn} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
 };
 
 export default Button;
