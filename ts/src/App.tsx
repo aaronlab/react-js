09e4ddbe2b6d1b22810045905ce3ff2389f18486
Aaron Lee <aaronlab.net@gmail.com>
2023-10-04 09:45:36 +0900

    form

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,10 +1,33 @@
-import Circle from "./Circle";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    console.log('On Submit')
+  };
+
   return (
     <div>
-      <Circle bgColor="white" borderColor="teal" />
-      <Circle bgColor="tomato" text="Hello!" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="user name"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
