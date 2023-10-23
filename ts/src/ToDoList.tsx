b3274f7940bdd056fdb4abf5fb047db6b603cd99
Aaron Lee <aaronlab.net@gmail.com>
2023-10-23 09:15:32 +0900

    React form

--- ts/src/ToDoList.tsx
+++ ts/src/ToDoList.tsx
@@ -1,24 +1,50 @@
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
+  const { register, watch, handleSubmit, formState } = useForm();
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
+  watch((value) => {});
+
+  const onValid = (data: any) => {
+    console.log(data);
   };
 
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  console.log(formState.errors);
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("userName", { required: true, minLength: 10 })}
+          placeholder="User Name"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "Password is required!",
+            },
+          })}
+          placeholder="Confirm Password"
+        />
         <button>Add</button>
       </form>
     </div>
