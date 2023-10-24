2a6510a78eb594ecbd90c116ec1d42d77af333af
Aaron Lee <aaronlab.net@gmail.com>
2023-10-24 09:59:20 +0900

    errors

--- ts/src/ToDoList.tsx
+++ ts/src/ToDoList.tsx
@@ -1,15 +1,44 @@
 import { useForm } from "react-hook-form";
 
+interface IFormData {
+  email: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  password: string;
+  password1: string;
+  serverError: string;
+}
+
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IFormData>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
 
-  watch((value) => {});
-
-  const onValid = (data: any) => {
+  const onValid = (data: IFormData) => {
     console.log(data);
-  };
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        {
+          message: "Passwords are not the same",
+        },
+        {
+          shouldFocus: true,
+        }
+      );
+    }
 
-  console.log(formState.errors);
+    setError("serverError", { message: "A server error occurred" });
+  };
 
   return (
     <div>
@@ -21,31 +50,76 @@ function ToDoList() {
         }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
-        <input {...register("firstName")} placeholder="First Name" />
         <input
-          {...register("lastName", { required: true })}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              message: "Invalid email",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+
+        <input
+          {...register("firstName", {
+            required: "First name is required",
+            validate: {
+              aaron: (value) => {
+                if (value.includes("aaron")) {
+                  return true;
+                }
+                return "Only aaron allowed";
+              },
+            },
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors.firstName?.message}</span>
+
+        <input
+          {...register("lastName", { required: "Last name is required" })}
           placeholder="Last Name"
         />
+        <span>{errors.lastName?.message}</span>
+
         <input
-          {...register("userName", { required: true, minLength: 10 })}
+          {...register("userName", {
+            required: "User name is required",
+            minLength: {
+              value: 10,
+              message: "User name shouldn't be less than 10",
+            },
+          })}
           placeholder="User Name"
         />
+        <span>{errors.userName?.message}</span>
+
         <input
-          {...register("password", { required: true, minLength: 5 })}
-          placeholder="Password"
-        />
-        <input
-          {...register("password1", {
-            required: true,
+          {...register("password", {
+            required: "Password is required",
             minLength: {
               value: 5,
-              message: "Password is required!",
+              message: "Password shouldn't be less than 5",
             },
+          })}
+          placeholder="Password"
+        />
+        <span>{errors.password?.message}</span>
+
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: 5,
           })}
           placeholder="Confirm Password"
         />
+        <span>{errors.password1?.message}</span>
+
         <button>Add</button>
+
+        <span>{errors.serverError?.message}</span>
       </form>
     </div>
   );
